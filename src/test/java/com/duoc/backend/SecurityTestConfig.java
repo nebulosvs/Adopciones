6b7995b153177305918d67c50e433d6765ca78ae
Sofía Medina <sofia.medina2113@gmail.com>
2026-05-08 18:00:56 -0400

    borrar remanentes

--- src/test/java/com/duoc/backend/SecurityTestConfig.java
+++ src/test/java/com/duoc/backend/SecurityTestConfig.java
@@ -3,7 +3,6 @@ package com.duoc.backend;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
